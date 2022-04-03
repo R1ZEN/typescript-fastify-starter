1d75140c2530f5dc3725ef6ef9e17cb204fe178e
Pavel Bely <poul-white@yandex.ru>
2022-04-03 17:52:44 +0300

    feat: Initial

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import Fastify from 'fastify';
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get('/', (request, reply) => {
+  reply.send({ hello: 'world' });
+});
+
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) throw err;
+});
